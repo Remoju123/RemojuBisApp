48db15666147bc21bb9f551f32ba1559c7a2c115
muramoto <mitsu@metier.co.jp>
2021-06-23 15:56:42 +0900

    upd0623 15:56

--- src/app/parts/confirm-message-dialog/confirm-message-dialog.component.ts
+++ src/app/parts/confirm-message-dialog/confirm-message-dialog.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { TranslateService } from "@ngx-translate/core";
+import { ComfirmDialogParam } from "../../class/common.class";
 
 @Component({
-  selector: 'app-confirm-message-dialog',
-  templateUrl: './confirm-message-dialog.component.html',
-  styleUrls: ['./confirm-message-dialog.component.scss']
+  selector: "app-confirm-message-dialog",
+  templateUrl: "./confirm-message-dialog.component.html",
+  styleUrls: ["./confirm-message-dialog.component.scss"]
 })
 export class ConfirmMessageDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService,
+    public dialogRef: MatDialogRef<ConfirmMessageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ComfirmDialogParam
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (!this.data.leftButton){
+      this.data.leftButton = "Cancel";
+    }
+    if (!this.data.rightButton){
+      this.data.rightButton = "OK";
+    }
   }
 
 }
