a19ca4910250704c8176d3c0195180524a55596d
bissetmm <bisset@metier.jp>
2021-10-06 16:54:21 +0900

    upd10061654

--- src/app/pages/mypage/mypage.component.ts
+++ src/app/pages/mypage/mypage.component.ts
@@ -28,22 +28,20 @@ export class MypageComponent implements OnInit, OnDestroy {
     this.activatedRoute.fragment.pipe(takeUntil(this.onDestroy$)).subscribe((fragment: any) => {
       if (fragment === "list"){
         // プラン一覧を表示
-        this.tabIndex = 2;
-        this.tabChange(2);
+        this.tabIndex = 0;
+        this.tabChange(0);
       } else if (fragment === "favorite") {
         // お気に入り一覧を表示
         this.tabIndex = 1;
         this.tabChange(1);
       } else if (fragment === "profile") {
-        this.tabIndex = 0;
-        this.tabChange(0);
+        this.tabIndex = 2;
+        this.tabChange(2);
       } else {
         this.tabIndex = 0;
         this.tabChange(0);
       }
     });
-
-
     
     // ヘッダプランの非表示通知
     this.common.onNotifyIsShowHeader(true);
@@ -55,14 +53,14 @@ export class MypageComponent implements OnInit, OnDestroy {
 
   // タブ変更(アクティブになっていないタブに画像を含めると幅が0になってしまう)
   tabChange($event: number){
-    if (!this.profile && $event === 0){
-      this.profile = true;
+    if (!this.planlist && $event === 0){
+      this.planlist = true;
     }
     if (!this.favorite && $event === 1) {
       this.favorite = true;
     }
-    if (!this.planlist && $event === 2){
-      this.planlist = true;
+    if (!this.profile && $event === 2){
+      this.profile = true;
     }
   }
 }
