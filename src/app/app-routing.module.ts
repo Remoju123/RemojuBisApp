71c77875fd5caf116e3619b5141cff1e8e2e0386
bissetmm <bisset@metier.jp>
2022-01-16 17:34:26 +0900

    upd01161734

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,15 +25,10 @@ const routes: Routes = [
         { path: "spots/detail/:id", loadChildren: () => import('./pages/spot-detail/spot-detail.module').then(m => m.SpotDetailModule) },
         { path: "plans/detail", loadChildren: () => import('./pages/plan-detail/plan-detail.module').then(m => m.PlanDetailModule) },
         { path: "plans/detail/:id", loadChildren: () => import('./pages/plan-detail/plan-detail.module').then(m => m.PlanDetailModule) },
-        //{ path: "mypage", component: MypageComponent, canActivate: [AuthGuard] },
         { path: "mypage", canLoad: [AuthGuard], loadChildren: () => import('./pages/mypage/mypage.module').then(m => m.MypageModule) },
         { path: "offcial", loadChildren: () => import('./pages/offcial/offcial.module').then(m => m.OffcialModule) },
-        //{ path: "user/:id", component: UserComponent },
-        // { path: 'privacy', loadChildren: () => import('./pages/offcial/offcial.module').then(m => m.OffcialModule) },
-        // { path: 'about', loadChildren: () => import('./pages/offcial/offcial.module').then(m => m.OffcialModule) },
-        // { path: 'guide', loadChildren: () => import('./pages/offcial/offcial.module').then(m => m.OffcialModule) },
         { path: "systemerror", component: SystemErrorComponent },
-        { path: '404', component: NotfoundComponent },
+        { path: '404', component: NotfoundComponent }
       ]
     },
     {
