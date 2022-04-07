dada230bfdd82db758cc37b1fd39b6a5f3200c69
yamazakisayuri <kermitsayuri@gmail.com>
2022-04-07 14:21:36 +0900

    REMOJU-465 プラン保存後にマイプラン一覧に反映されない

--- src/app/pages/mypage-planlist/mypage-planlist.component.ts
+++ src/app/pages/mypage-planlist/mypage-planlist.component.ts
@@ -113,7 +113,7 @@ export class MypagePlanListComponent implements OnInit, OnDestroy, AfterViewChec
     // 保存通知
     this.myplanService.PlanUserSaved$.pipe(takeUntil(this.onDestroy$)).subscribe(x => {
       // 保存されたプランが表示されている場合、最新を取得
-      this.getPlanListDetail(x.planUserId);
+      this.getPlanListDetail(x.planUserId, true);
     });
   }
 
@@ -288,46 +288,58 @@ export class MypagePlanListComponent implements OnInit, OnDestroy, AfterViewChec
   }
 
   // マイページプラン一覧詳細取得
-  getPlanListDetail(planUserId: number = 0, isComplement: boolean = false) {
+  async getPlanListDetail(planUserId: number = 0, isFirst: boolean = false) {
     let startIndex = (this.p - 1) * this.limit;
     this.end = startIndex + this.limit;
     if (this.rows.length - startIndex < this.limit) {
       this.end = this.rows.length;
     }
-    if (isComplement) {
+    if (isFirst) {
       startIndex = 0;
     }
+    const result = [];
     for (let i = startIndex; i < this.end; i++) {
-      if (planUserId && planUserId !== this.rows[i].planUserId){
+      if (planUserId > 0 && planUserId !== this.rows[i].planUserId){
         continue;
       }
-      if (this.rows[i].isDetail) {
+      if (planUserId === 0 && this.rows[i].isDetail) {
         this.details$ = this.rows.slice(0, this.end);
         continue;
       }
-      this.mypagePlanListService
-        .getMypagePlanListDetail(this.rows[i])
-        .pipe(takeUntil(this.onDestroy$))
-        .subscribe(d => {
-            const idx = this.rows.findIndex(v => v.planUserId === d.planUserId);
-            this.rows[idx] = d;
-            if (d.spots && d.spots.length > 0){
-              this.rows[idx].spots = d.spots.map((x, i) => {
-                if (x.type === 1){
-                  x.spotName = this.commonService.isValidJson(x.spotName, this.lang);
-                }
-                return x;
-              }, []);
-            }
-            this.details$ = this.rows.slice(0, this.end);
-            if (i === this.end -1 && isPlatformServer(this.platformId)) {
-              this.setTransferState();
-            }
-        });
+      result.push(this.getDetail(i));
     }
+
     if (planUserId === 0) {
       this.p++;
     }
+
+    await Promise.all(result);
+
+    if (planUserId === 0 && isPlatformServer(this.platformId)) {
+      this.setTransferState();
+    }
+  }
+
+  getDetail(i: number) {
+    return new Promise((resolve) => {
+      this.mypagePlanListService
+      .getMypagePlanListDetail(this.rows[i])
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(d => {
+          const idx = this.rows.findIndex(v => v.planUserId === d.planUserId);
+          this.rows[idx] = d;
+          if (d.spots && d.spots.length > 0){
+            this.rows[idx].spots = d.spots.map((x, i) => {
+              if (x.type === 1){
+                x.spotName = this.commonService.isValidJson(x.spotName, this.lang);
+              }
+              return x;
+            }, []);
+          }
+          this.details$ = this.rows.slice(0, this.end);
+          resolve(true);
+      });
+    });
   }
 
   cacheRecoveryDataSet() {
