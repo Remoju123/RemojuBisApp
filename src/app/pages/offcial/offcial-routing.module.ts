09fc6a83c5dc4a85a942f22b131fed34a07fd5d3
Mitsuharu Muramoto <bisset@metier.jp>
2022-10-04 15:36:12 +0900

    upd10041536

--- src/app/pages/offcial/offcial-routing.module.ts
+++ src/app/pages/offcial/offcial-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { PrivacyComponent } from './privacy/privacy.component';
-import { GuideComponent } from './guide/guide.component';
-//import { AboutComponent } from './about/about.component';
-import { OffcialComponent } from './offcial.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: 'privacy', pathMatch: 'full' },
   { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-  { path: 'guide', component: GuideComponent },
-  { path: 'privacy', component: PrivacyComponent },
+  { path: 'guide', loadChildren: () => import('./guide/guide.module').then(m => m.GuideModule) },
+  { path: 'privacy', loadChildren: () => import('./privacy/privacy.module').then(m => m.PrivacyModule) },
   { path: 'contact', component: ContactFormComponent }
 ];
 
