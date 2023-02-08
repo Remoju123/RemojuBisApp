3ffa68e4c116cbfd8972474bf62c9b015c5e33fc
bisset <bisset@metier.jp>
2023-02-09 07:33:54 +0900

    upd0733

---
+++ src/app/pages/plan-box/plan-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-box',
+  templateUrl: './plan-box.component.html',
+  styleUrls: ['./plan-box.component.scss']
+})
+export class PlanBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
