b46c4996d12ccd10868938cf0d34bbecbac464de
bissetmm <bisset@metire.jp>
2023-03-21 08:39:52 +0900

    ng update @angular/cli@14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LanguageComponent } from "./language/language.component";
-import { BlankComponent } from "./layout/blank/blank.component";
+import { LanguageComponent } from './language/language.component';
+import { BlankComponent } from './layout/blank/blank.component';
 import { RootComponent } from './layout/root/root.component';
 import { AuthGuard } from './auth.guard';
 
@@ -13,48 +13,107 @@ import { SystemErrorComponent } from './pages/system-error/system-error.componen
 
 const routes: Routes = [
   {
-    path: ":lang",
+    path: ':lang',
     component: LanguageComponent,
-    children: [{
-      path: "",
-      component: RootComponent,
-      children: [
-        { path: "", redirectTo: "/ja/planspot", pathMatch: "full" },
-        { path: "planspot", loadChildren: () => import('./pages/planspot/planspot.module').then(m => m.PlanspotModule) },
-        { path: "spots/detail", loadChildren: () => import('./pages/spot-detail/spot-detail.module').then(m => m.SpotDetailModule) },
-        { path: "spots/detail/:id", loadChildren: () => import('./pages/spot-detail/spot-detail.module').then(m => m.SpotDetailModule) },
-        { path: "plans/detail", loadChildren: () => import('./pages/plan-detail/plan-detail.module').then(m => m.PlanDetailModule) },
-        { path: "plans/detail/:id", loadChildren: () => import('./pages/plan-detail/plan-detail.module').then(m => m.PlanDetailModule) },
-        { path: "userplans/:id", loadChildren: () => import('./pages/user-plan-list/user-plan-list.module').then(m => m.UserPlanListModule) },
-        { path: "mypage", canActivate: [AuthGuard], loadChildren: () => import('./pages/mypage/mypage.module').then(m => m.MypageModule) },
-        { path: "mypage#list", canActivate: [AuthGuard], loadChildren: () => import('./pages/mypage/mypage.module').then(m => m.MypageModule) },
-        { path: "offcial", loadChildren: () => import('./pages/offcial/offcial.module').then(m => m.OffcialModule) },
-        { path: "systemerror", component: SystemErrorComponent },
-        { path: '404', component: NotfoundComponent }
-      ]
-    },
-    {
-      path: "",
-      component: BlankComponent,
-      children: [
-        { path: "top", component: TopComponent }//,
-        //{ path: "map", component: MapComponent }
-      ]
-    },
-    { path: "", redirectTo: "/ja/planspot", pathMatch: "full" }
+    children: [
+      {
+        path: '',
+        component: RootComponent,
+        children: [
+          { path: '', redirectTo: '/ja/planspot', pathMatch: 'full' },
+          {
+            path: 'planspot',
+            loadChildren: () =>
+              import('./pages/planspot/planspot.module').then(
+                (m) => m.PlanspotModule
+              ),
+          },
+          {
+            path: 'spots/detail',
+            loadChildren: () =>
+              import('./pages/spot-detail/spot-detail.module').then(
+                (m) => m.SpotDetailModule
+              ),
+          },
+          {
+            path: 'spots/detail/:id',
+            loadChildren: () =>
+              import('./pages/spot-detail/spot-detail.module').then(
+                (m) => m.SpotDetailModule
+              ),
+          },
+          {
+            path: 'plans/detail',
+            loadChildren: () =>
+              import('./pages/plan-detail/plan-detail.module').then(
+                (m) => m.PlanDetailModule
+              ),
+          },
+          {
+            path: 'plans/detail/:id',
+            loadChildren: () =>
+              import('./pages/plan-detail/plan-detail.module').then(
+                (m) => m.PlanDetailModule
+              ),
+          },
+          {
+            path: 'userplans/:id',
+            loadChildren: () =>
+              import('./pages/user-plan-list/user-plan-list.module').then(
+                (m) => m.UserPlanListModule
+              ),
+          },
+          {
+            path: 'mypage',
+            canActivate: [AuthGuard],
+            loadChildren: () =>
+              import('./pages/mypage/mypage.module').then(
+                (m) => m.MypageModule
+              ),
+          },
+          {
+            path: 'mypage#list',
+            canActivate: [AuthGuard],
+            loadChildren: () =>
+              import('./pages/mypage/mypage.module').then(
+                (m) => m.MypageModule
+              ),
+          },
+          {
+            path: 'offcial',
+            loadChildren: () =>
+              import('./pages/offcial/offcial.module').then(
+                (m) => m.OffcialModule
+              ),
+          },
+          { path: 'systemerror', component: SystemErrorComponent },
+          { path: '404', component: NotfoundComponent },
+        ],
+      },
+      {
+        path: '',
+        component: BlankComponent,
+        children: [
+          { path: 'top', component: TopComponent }, //,
+          //{ path: "map", component: MapComponent }
+        ],
+      },
+      { path: '', redirectTo: '/ja/planspot', pathMatch: 'full' },
     ],
   },
-  { path: "", redirectTo: "/ja/planspot", pathMatch: "full" },
-  { path: "**", component: NotfoundComponent }
+  { path: '', redirectTo: '/ja/planspot', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: "enabled",
-    anchorScrolling: "enabled",
-    relativeLinkResolution: 'legacy',
-    initialNavigation: 'enabled'
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      relativeLinkResolution: 'legacy',
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
