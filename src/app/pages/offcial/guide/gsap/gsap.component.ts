eadc9b1a7f1dfaeb7509b971f6113901b64bd9e5
bissetmm <bisset@metire.jp>
2023-03-24 18:12:28 +0900

    gsap init

---
+++ src/app/pages/offcial/guide/gsap/gsap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gsap',
+  templateUrl: './gsap.component.html',
+  styleUrls: ['./gsap.component.css']
+})
+export class GsapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
