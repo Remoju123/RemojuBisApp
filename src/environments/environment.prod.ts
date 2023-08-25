5910e54591b7affb4711329d7f3769a22c8ac2ea
bissetmm <bisset@metire.jp>
2023-08-25 15:32:48 +0900

    env prod change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -20,12 +20,12 @@ export const environment = {
   host: locationhost + "/app",
   location: locationhost + "/",
   openidConf:"https://remojuauth.b2clogin.com/remojuauth.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_signup_signin",
-  backend: "https://remoju-api-v2-dev.azurewebsites.net",
+  backend: "https://remoju-api-v3.azurewebsites.net",
   blobUrl: "https://ik.imagekit.io/2qejhoz7bda",
   // pwdreset:"https://remojuauth.b2clogin.com/remojuauth.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_PasswordReset&client_id=3e5bffaf-86d7-4a4c-bcde-6ba4d1cb52d3&nonce=defaultNonce&redirect_uri=" + window.location.origin + "&scope=openid&response_type=id_token&prompt=login",
   pwdreset:"",
   indexeddbValidityPeriod: 14,
-  apiKey: "AIzaSyC8Kodz9bFEhBGkTlBF0PeOk6QT9hpL9hk",
+  apiKey: "AIzaSyDrlzKsfxFZWRFkANrL394ogqxyOTfscrQ",
   analytics: {
     id: 'G-WD8D3WTZS4'
   }
